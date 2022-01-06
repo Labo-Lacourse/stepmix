849c326167c38d6db4129fc51e8d5627ff615b5e
sachaMorin <morin.sacha@gmail.com>
2022-01-06 09:08:32 -0500

    Simulated Gaussian Dataset

--- lca/datasets.py
+++ lca/datasets.py
@@ -36,3 +36,35 @@ def data_generation_Bakk(sample_size, sep_level, random_state=None):
     Z = np.array([rng.normal(mus[Xidx[i]], sigmas[Xidx[i]]) for i in range(sample_size)]).reshape(sample_size, D)
 
     return Y, Z
+
+
+def data_generation_gaussian(sample_size, random_state=None):
+    rng = check_random_state(random_state)
+
+    C = 4  # Number of latent classes
+    K = 2  # Dimension of the gaussian measurement
+    D = 2  # Dimension of the response variable Z_o
+
+    # True parameters
+    rho = np.ones(C) / C  # Uniform prior on latent class
+
+    # Evenly spaced gaussian on a high dimensional diagonal
+    mus = np.array([[1, 1], [-1, 1], [1, -1], [-1, -1]]) * 10
+
+    # unit variance for all gaussians
+    # sigmas = np.ones((K,))
+    pis = np.array([
+        [.1, .9],
+        [.9, .1],
+        [.9, .1],
+        [.1, .9],
+    ]).T
+
+    # Data generation
+    X = rng.multinomial(1, rho, size=sample_size)  # one-hot: X[i,c]=1 iff X[i]=c
+    Xidx = np.dot(np.array([c for c in range(C)]), X.T)  # index: Xidx[i]=c
+    Z = np.array([0 + (rng.rand() < pis[d, Xidx[i]]) for i in range(sample_size) for d in range(D)]).reshape(
+        sample_size, D)
+    Y = np.array([rng.multivariate_normal(mus[Xidx[i]], np.eye(K)) for i in range(sample_size)])
+
+    return Y, Z
