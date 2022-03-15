55b7f42ec024634c8a04624755b6948fef3f2c4d
sachaMorin <morin.sacha@gmail.com>
2022-03-15 15:04:56 -0400

    Move parameter getting/setting to Emission class

--- lca/emission.py
+++ lca/emission.py
@@ -2,6 +2,8 @@
 
 Encapsulate the M-step and log-likelihood computations of different conditional emission models."""
 from abc import ABC, abstractmethod
+import copy
+
 from sklearn.utils.validation import check_random_state
 from sklearn.mixture import GaussianMixture
 from scipy.stats import multivariate_normal
@@ -14,6 +16,9 @@ class Emission(ABC):
     def __init__(self, n_components, random_state):
         self.n_components = n_components
         self.random_state = random_state
+
+        # Dict including all parameters for estimation
+        self.parameters = dict()
 
     def check_parameters(self):
         check_int(n_components=self.n_components)
@@ -37,6 +42,12 @@ class Emission(ABC):
             random_state = check_random_state(random_state)
         return random_state
 
+    def get_parameters(self):
+        return copy.deepcopy(self.parameters)
+
+    def set_parameters(self, params):
+        self.parameters = params
+
     @abstractmethod
     def m_step(self, X, log_resp):
         raise NotImplementedError
@@ -45,13 +56,47 @@ class Emission(ABC):
     def log_likelihood(self, X):
         raise NotImplementedError
 
-    @abstractmethod
-    def get_parameters(self):
-        raise NotImplementedError
 
-    @abstractmethod
-    def set_parameters(self, params):
-        raise NotImplementedError
+class Bernoulli(Emission):
+    def __init__(self, clip_eps=1e-15, n_components=2, random_state=None):
+        super().__init__(n_components=n_components, random_state=random_state)
+        self.clip_eps = clip_eps
+
+    def check_parameters(self):
+        super().check_parameters()
+        check_nonneg(clip_eps=self.clip_eps)
+
+    def m_step(self, X, log_resp):
+        resp = np.exp(log_resp)
+        pis = X.T @ resp
+        pis /= resp.sum(axis=0, keepdims=True)
+        pis = np.clip(pis, self.clip_eps, 1 - self.clip_eps)  # avoid probabilities 0 or 1
+        self.parameters['pis'] = pis
+
+    def log_likelihood(self, X):
+        # compute log emission probabilities
+        pis = np.clip(self.parameters['pis'], self.clip_eps, 1 - self.clip_eps)  # avoid probabilities 0 or 1
+        log_eps = X @ np.log(pis) + (1 - X) @ np.log(1 - pis)
+
+        return log_eps
+
+
+class GaussianUnit(Emission):
+    """sklearn.mixture.GaussianMixture does not have an implementation for fixed unit variance, so we provide one."""
+
+    def __init__(self, n_components=2, random_state=None):
+        super().__init__(n_components=n_components, random_state=random_state)
+
+    def m_step(self, X, log_resp):
+        resp = np.exp(log_resp)
+        self.parameters['means'] = (resp[..., np.newaxis] * X[:, np.newaxis, :]).sum(axis=0) / resp.sum(axis=0, keepdims=True).T
+
+    def log_likelihood(self, X):
+        n, D = X.shape
+        log_eps = np.zeros((n, self.n_components))
+        for c in range(self.n_components):
+            log_eps[:, c] = multivariate_normal.logpdf(x=X, mean=self.parameters['means'][c], cov=1)
+        return log_eps
 
 
 class Gaussian(Emission):
@@ -63,6 +108,11 @@ class Gaussian(Emission):
         self.reg_covar = reg_covar
 
         # Actual parameters
+        # We spoof the sklearn GaussianMixture class
+        # This is not needed for typical children of the Emission class. We do this only to be compatible
+        # with the sklearn GaussianMixture machinery.
+        # Normally you should keep all parameters in a dict under self.parameters
+        self.parameters = None
         self.means_ = None
         self.covariances_ = None
         self.precisions_cholesky_ = None
@@ -95,9 +145,12 @@ class Gaussian(Emission):
         return GaussianMixture._estimate_log_prob(self, X)
 
     def get_parameters(self):
-        return dict(means=self.means_, covariances=self.covariances_, precisions_cholesky=self.precisions_cholesky_)
+        return dict(means=self.means_.copy(), covariances=self.covariances_.copy(), precisions_cholesky=self.precisions_cholesky_.copy())
 
     def set_parameters(self, params):
+        # We spoof the sklearn GaussianMixture class
+        # This is not needed for typical children of the Emission class. We do this only to be compatible
+        # with the sklearn GaussianMixture machinery.
         # This will update self.means_, self.covariances_ and self.precisions_cholesky_
         GaussianMixture._set_parameters(self,
                                         (None, params['means'], params['covariances'], params['precisions_cholesky']))
@@ -131,64 +184,7 @@ class GaussianTied(Gaussian):
         super().__init__(covariance_type='tied', **kwargs)
 
 
-class GaussianUnit(Emission):
-    """sklearn.mixture.GaussianMixture does not have an implementation for fixed unit variance, so we provide one."""
-
-    def __init__(self, n_components=2, random_state=None):
-        super().__init__(n_components=n_components, random_state=random_state)
-        self.means = None
-
-    def m_step(self, X, log_resp):
-        resp = np.exp(log_resp)
-        self.means = (resp[..., np.newaxis] * X[:, np.newaxis, :]).sum(axis=0) / resp.sum(axis=0, keepdims=True).T
-
-    def log_likelihood(self, X):
-        n, D = X.shape
-        log_eps = np.zeros((n, self.n_components))
-        for c in range(self.n_components):
-            log_eps[:, c] = multivariate_normal.logpdf(x=X, mean=self.means[c], cov=1)
-        return log_eps
-
-    def get_parameters(self):
-        return dict(means=self.means)
-
-    def set_parameters(self, params):
-        self.means = params['means']
-
-
-class Bernoulli(Emission):
-    def __init__(self, clip_eps=1e-15, n_components=2, random_state=None):
-        super().__init__(n_components=n_components, random_state=random_state)
-        self.clip_eps = clip_eps
-        self.pis = None
-
-    def check_parameters(self):
-        super().check_parameters()
-        check_nonneg(clip_eps=self.clip_eps)
-
-    def m_step(self, X, log_resp):
-        resp = np.exp(log_resp)
-        pis = X.T @ resp
-        pis /= resp.sum(axis=0, keepdims=True)
-        pis = np.clip(pis, self.clip_eps, 1 - self.clip_eps)  # avoid probabilities 0 or 1
-        self.pis = pis
-
-    def log_likelihood(self, X):
-        # compute log emission probabilities
-        pis = np.clip(self.pis, self.clip_eps, 1 - self.clip_eps)  # avoid probabilities 0 or 1
-        log_eps = X @ np.log(pis) + (1 - X) @ np.log(1 - pis)
-
-        return log_eps
-
-    def get_parameters(self):
-        return dict(pis=self.pis)
-
-    def set_parameters(self, params):
-        self.pis = params['pis']
-
-
 EMISSION_DICT = {
-    'gaussian': Gaussian,
     'gaussian_unit': GaussianUnit,
     'gaussian_full': GaussianFull,
     'gaussian_spherical': GaussianSpherical,
