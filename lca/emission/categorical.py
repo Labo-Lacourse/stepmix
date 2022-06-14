782b6958c1aac139e758f9c784d70991a6274f47
Robin Legault <robin.legault@umontreal.ca>
2022-06-14 01:00:29 -0400

    Add multinoulli emission model

--- lca/emission/categorical.py
+++ lca/emission/categorical.py
@@ -16,23 +16,32 @@ class Bernoulli(Emission):
         # compute log emission probabilities
         pis = np.clip(self.parameters['pis'], 1e-15, 1 - 1e-15)  # avoid probabilities 0 or 1
         log_eps = X @ np.log(pis) + (1 - X) @ np.log(1 - pis)
-
         return log_eps
 
     def sample(self, class_no, n_samples):
         feature_weights = self.parameters['pis'][:, class_no].reshape((1, -1))
-        c = feature_weights.shape[1]
-        X = (self.random_state.uniform(size=(n_samples, c)) < feature_weights).astype(int)
+        K = feature_weights.shape[1]  #number of features
+        X = (self.random_state.uniform(size=(n_samples, K)) < feature_weights).astype(int)
         return X
 
 class Multinoulli(Emission):
     """Multinoulli (categorical) emission model."""
 
     def m_step(self, X, resp):
-        raise NotImplementedError("Multinoulli model is not implemented.")
+        pis = np.swapaxes((X.T @ resp).T, 0, 1)
+        pis /= np.swapaxes((X.T @ resp).T.sum(axis=2, keepdims=True), 0, 1)
+        pis = np.clip(pis, 1e-15, 1 - 1e-15)  # avoid probabilities 0 or 1
+        self.parameters['pis'] = pis
 
     def log_likelihood(self, X):
-        raise NotImplementedError("Multinoulli model is not implemented.")
+        # compute log emission probabilities
+        pis = np.clip(self.parameters['pis'], 1e-15, 1 - 1e-15)  # avoid probabilities 0 or 1
+        n, K, L = X.shape    # n individuals, K features, L possible outcomes for each multinoulli
+        K, C, L = pis.shape  # C latent classes
+        log_eps = np.reshape(X, (n, K*L)) @ np.reshape(np.swapaxes(np.log(pis), 0, 1), (C, K*L)).T
+        return log_eps
 
     def sample(self, class_no, n_samples):
-        raise NotImplementedError("Multinoulli model is not implemented.")
+        feature_weights = self.parameters['pis'][:, class_no, :]
+        X = np.array([self.random_state.multinomial(1, feature_weights[k], size=n_samples) for k in range(K)])
+        return X
