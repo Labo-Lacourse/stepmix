67660863bb4c0565f775b512abf4b7add67e2282
sachaMorin <morin.sacha@gmail.com>
2022-07-03 21:02:06 -0400

    New gaussian diag dataset with nan_ratio option

--- lca/datasets.py
+++ lca/datasets.py
@@ -1,4 +1,6 @@
 """Various synthetic datasets."""
+import warnings
+
 import numpy as np
 from sklearn.utils.validation import check_random_state
 from scipy.special import softmax
@@ -142,7 +144,7 @@ def data_bakk_covariate(n_samples, sep_level, n_mm=6, random_state=None):
 
     # Latent class (Ground truth class membership)
     # probabilistic assignment ( realizations from the distribution of the r.v label_i|Y_i )
-    probas = softmax(logits, axis=1) #probas[n,c] = probability for unit n to be assigned to class c
+    probas = softmax(logits, axis=1)  # probas[n,c] = probability for unit n to be assigned to class c
     cumul_probas = np.cumsum(probas, axis=1)
     bool_tab = (cumul_probas - np.tile(rng.rand(n_samples), (n_classes, 1)).T) >= 0
     labels = -np.sum(bool_tab, axis=1) + n_classes
@@ -228,5 +230,89 @@ def data_generation_gaussian(n_samples, sep_level, n_mm=6, random_state=None):
                     random_state=random_state)
     generator.set_parameters(params)
     X, Y, labels = generator.sample(n_samples)
+
+    return X, Y, labels
+
+
+# Data generation: Simulated problems from IFT6269 Hwk 4
+def data_gaussian_diag(n_samples, sep_level, n_mm=6, random_state=None, nan_ratio=0.0):
+    """Bakk binary measurement model with 2D diagonal gaussian structural model.
+
+    Optionally, a random proportion of values can be replaced with missing values to test FIML models.
+
+    Parameters
+    ----------
+    n_samples : int
+        Number of samples.
+    sep_level : float
+        Separation level in the measurement data. Use .7, .8 or .9 for the paper simulation.
+    n_mm: int
+        Number of features in the measurement model. Use 6 for the paper simulation.
+    random_state: int
+        Random state.
+    nan_ratio: float
+        Ratio of values to replace with missing values.
+
+    Returns
+    -------
+    X : ndarray of shape (n_samples, n_features)
+        Measurement samples.
+    Y : ndarray of shape (n_samples, n_features)
+        Structural samples.
+    labels : ndarray of shape (n_samples,)
+        Ground truth class membership.
+
+    """
+    n_classes = 3  # Number of latent classes
+    n_sm = 2  # Dimensions of the response variable Zo
+
+    # True parameters
+    # rho[c] = p(X=c)
+    rho = np.ones(n_classes) / n_classes
+
+    # mus[k] = E[Z0|X=c]
+    # mus[k] = E[Z0|X=c]
+    mus = np.array([[0.0, 0.0],
+                    [5.0, 5.0],
+                    [-5.0, -5.0]])
+
+    # sigmas[k] = V[Z0|X=c]
+    sigmas = np.array([[[1.0, 0.0], [0.0, 1.0]],
+                       [[2.0, 0.0], [0.0, 2.0]],
+                       [[1.0, 0.0], [0.0, 3.0]]])
+
+    # pis[k,c] = p(Yk=1|X=c)
+    # sep_level = 0.9 #0.9->high, 0.8->medium, 0.7->low
+    pis = bakk_measurements(n_classes, n_mm, sep_level)
+
+    # Model parameters
+    params = dict(
+        weights=rho,
+        measurement=dict(pis=pis),
+        structural=dict(means=mus, covariances=sigmas),
+        measurement_in=n_mm,
+        structural_in=n_sm,
+    )
+
+    # Sample data
+    generator = LCA(n_components=n_classes, measurement='bernoulli', structural='gaussian_full',
+                    random_state=random_state)
+    generator.set_parameters(params)
+    X, Y, labels = generator.sample(n_samples)
+
+    # Drop some values
+    if nan_ratio:
+        rng = np.random.default_rng(random_state)
+
+        observed_mask = rng.random((n_samples, n_mm + n_sm)) > nan_ratio
+
+        if not observed_mask.sum(axis=1).all():
+            warnings.warn('Some samples are completely unobserved. This will likely result in downstream errors. Reduce the nan_ratio or try another'
+                          'seed.')
+
+        mm_mask, sm_mask = observed_mask[:, :n_mm], observed_mask[:, n_mm:]
+
+        X = np.where(mm_mask, X, np.nan)
+        Y = np.where(sm_mask, Y, np.nan)
 
     return X, Y, labels
