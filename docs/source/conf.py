049d0a4e027742c8069b62ca91efe96243da90cd
sachaMorin <morin.sacha@gmail.com>
2023-04-30 17:14:28 -0400

    bump version 1.0.0 -> 1.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ copyright = "2022, Labo-Lacourse"
 author = "Sacha Morin, Robin Legault"
 
 release = "0.0"
-version = "1.0.0"
+version = "1.0.1"
 
 
 # -- General configuration
--- stepmix/__init__.py
+++ stepmix/__init__.py
@@ -1,6 +1,6 @@
 """A Python package for stepwise estimation of latent class models with measurement and structural components"""
 
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from stepmix.stepmix import StepMix
