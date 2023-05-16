6e6569835ddcf7f883bd191d5e9e2ca4f5b50c0c
sachaMorin <morin.sacha@gmail.com>
2023-05-16 10:44:28 -0400

    Better test for sample weights

---
+++ test/test_sample_weight.py
@@ -0,0 +1,35 @@
+"""Test sample weights."""
+import numpy as np
+import pytest
+from stepmix.stepmix import StepMix
+
+def test_default_weights(data, kwargs):
+    """Running StepMix without sample weights should be the same as running StepMix with unit weights."""
+    X, Y = data
+
+    model_1 = StepMix(**kwargs)
+    model_1.fit(X, Y)
+    ll_1 = model_1.score(X, Y)
+
+    model_2 = StepMix(**kwargs)
+    sample_weight = np.ones(X.shape[0])
+    model_2.fit(X, Y, sample_weight=sample_weight)
+    ll_2 = model_2.score(X, Y, sample_weight=sample_weight)
+
+    assert ll_1 == ll_2
+
+def test_different_weights(data, kwargs):
+    """Running StepMix with different weights should result in a different likelihood."""
+    X, Y = data
+
+    model_1 = StepMix(**kwargs)
+    model_1.fit(X, Y)
+    ll_1 = model_1.score(X, Y)
+
+    model_2 = StepMix(**kwargs)
+    sample_weight = np.ones(X.shape[0])
+    sample_weight[0] = 100
+    model_2.fit(X, Y, sample_weight=sample_weight)
+    ll_2 = model_2.score(X, Y, sample_weight=sample_weight)
+
+    assert ll_1 == ll_2
