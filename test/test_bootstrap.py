74dd329287493082db53ecc19a2b6c9e9526f8e3
sachaMorin <morin.sacha@gmail.com>
2023-08-09 13:39:42 -0400

    Tests for new bootstrap functionalities

--- test/test_bootstrap.py
+++ test/test_bootstrap.py
@@ -115,3 +115,63 @@ def test_bootstrap_df(data_nested, kwargs_nested):
     model_1 = StepMix(**kwargs_nested)
     model_1.fit(data_nested, data_nested)
     model_1.bootstrap(data_nested, data_nested, n_repetitions=3)
+
+@pytest.mark.parametrize("parametric", [True, False])
+def test_boostrap_seed(data_nested, kwargs_nested, parametric):
+    """Call bootstrap twice and make sure the results are the same if seeded."""
+    data_nested = pd.DataFrame(data_nested)
+
+    model_1 = StepMix(**kwargs_nested) # Base model is seeded
+
+    model_1.fit(data_nested, data_nested)
+
+    _, stats = model_1.bootstrap(data_nested, data_nested, n_repetitions=10, parametric=parametric)
+
+    _, stats2 = model_1.bootstrap(data_nested, data_nested, n_repetitions=10, parametric=parametric)
+
+    assert np.all(stats == stats2)
+
+def test_boostrap_seed_sampler(data_nested, kwargs_nested):
+    """Call bootstrap twice with sampler and make sure the results are the same if seeded."""
+    data_nested = pd.DataFrame(data_nested)
+
+    model_1 = StepMix(**kwargs_nested) # Base model is seeded
+    model_1.fit(data_nested, data_nested)
+
+    kwargs_nested['n_components'] = 2
+    sampler = StepMix(**kwargs_nested)
+    sampler.fit(data_nested, data_nested)
+
+    _, stats0 = model_1.bootstrap(data_nested, data_nested, n_repetitions=10, parametric=True, sampler=model_1)
+
+    _, stats1 = model_1.bootstrap(data_nested, data_nested, n_repetitions=10, parametric=True, sampler=sampler)
+
+    _, stats2 = model_1.bootstrap(data_nested, data_nested, n_repetitions=10, parametric=True, sampler=sampler)
+
+    assert np.all(stats0 == stats1)
+    assert np.all(stats1 == stats2)
+
+@pytest.mark.parametrize("parametric", [True, False])
+def test_boostrap_mm(data_nested, kwargs_nested, parametric):
+    """Call bootstrap with mm only and make sure it does not raise errors."""
+    data_nested = pd.DataFrame(data_nested)
+
+    model_1 = StepMix(**kwargs_nested)
+
+    model_1.fit(data_nested)
+
+    model_1.bootstrap(data_nested, n_repetitions=10, parametric=parametric)
+
+def test_boostrap_mm_sampler(data_nested, kwargs_nested):
+    """Call sampler bootstrap with mm only and make sure it does not raise errors."""
+    data_nested = pd.DataFrame(data_nested)
+
+    model_1 = StepMix(**kwargs_nested)
+
+    model_1.fit(data_nested)
+
+    kwargs_nested['n_components'] = 2
+    sampler = StepMix(**kwargs_nested)
+    sampler.fit(data_nested)
+
+    model_1.bootstrap(data_nested, n_repetitions=10, parametric=True, sampler=sampler)
