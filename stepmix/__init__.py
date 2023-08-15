7c31c7aa4d4ddcdfaf1e9f233554a919c4806b6f
sachaMorin <morin.sacha@gmail.com>
2023-08-15 17:06:52 -0400

    bump version 2.0.0 -> 2.1.0

--- stepmix/__init__.py
+++ stepmix/__init__.py
@@ -1,6 +1,6 @@
 """A Python package for stepwise estimation of latent class models with measurement and structural components"""
 
 
-__version__ = "2.0.0"
+__version__ = "2.1.0"
 
 from stepmix.stepmix import StepMix
